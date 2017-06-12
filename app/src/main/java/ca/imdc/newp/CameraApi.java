e2370ee9027ea7317a7f5d6a753ee83ab3156c77
pkkamboj <pkamboj@ryerson.ca>
2017-06-11 22:52:15 -0400

    Camera orientation is preveiwing and recording correctly for other camera

--- app/src/main/java/ca/imdc/newp/CameraApi.java
+++ app/src/main/java/ca/imdc/newp/CameraApi.java
@@ -30,6 +30,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -83,6 +85,7 @@ public class CameraApi extends AppCompatActivity {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
             setupCamera(i, i1);
+            transformImage(i, i1);
             connectCamera();
 
         }
@@ -272,6 +275,7 @@ public class CameraApi extends AppCompatActivity {
 
         if (mTextureView.isAvailable()) {
             setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+            transformImage(mTextureView.getWidth(), mTextureView.getHeight());
             connectCamera();
         } else {
             mTextureView.setSurfaceTextureListener(mSurfaceListener);
@@ -386,6 +390,31 @@ public class CameraApi extends AppCompatActivity {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private void transformImage(int width, int height){
+       /* if (mPreviewSize == null || mTextureView == null);
+        {
+            return;
+        }*/
+
+        Matrix matrix = new Matrix ();
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        RectF textureRectF = new RectF(0, 0, width, height);
+        RectF previewRectF = new RectF(0, 0, mPreviewSize.getHeight(), mPreviewSize.getWidth());
+        float centerX = textureRectF.centerX();
+        float centerY = textureRectF.centerY();
+
+        if(rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270){
+            previewRectF.offset(centerX - previewRectF.centerX(), centerY - previewRectF.centerY());
+            matrix.setRectToRect(textureRectF, previewRectF, Matrix.ScaleToFit.FILL);
+            float scale = Math.max(((float)width / mPreviewSize.getWidth()), ((float)height / mPreviewSize.getHeight()));
+            matrix.postScale(scale, scale, centerX, centerY);
+            matrix.postRotate(90 + (rotation - 2), centerX, centerY);
+        }
+
+        mTextureView.setTransform(matrix);
+
     }
 
     private void connectCamera() {
@@ -511,9 +540,18 @@ public class CameraApi extends AppCompatActivity {
     }
 
     private static int sensorToDeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation){
+        int result;
         int sensorOrientation = cameraCharacteristics.get(cameraCharacteristics.SENSOR_ORIENTATION);
         deviceOrientation = ORIENTATIONS.get(deviceOrientation);
-        return (sensorOrientation + deviceOrientation + 360) % 360;
+        if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                CameraCharacteristics.LENS_FACING_FRONT){
+            result = (sensorOrientation + deviceOrientation) % 360;
+            result = (360 - result) % 360; //the mirror
+        } else {
+            result = (sensorOrientation + deviceOrientation + 360) % 360;
+        }
+
+        return result;
     }
 
     private static Size chooseOptimalSize(Size[] choices, int width, int height){
